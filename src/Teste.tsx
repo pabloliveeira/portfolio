3d9febf8e31575f575b59dc8d4e32694e4e72f92
Pablo Oliveira <pablo230197@gmail.com>
2024-07-10 21:24:37 -0300

    first commit

---
+++ src/Teste.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Botao = styled.button`
+  background-color: red;
+`
+
+function Teste() {
+  return <Botao>Clique Aqui</Botao>
+}
+
+export default Teste
